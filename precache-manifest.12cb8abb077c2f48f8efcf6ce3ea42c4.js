101937b70b6f75ea546d905554e0984c76781d4a
Branko Dimitrijoski <brankodimitrioski@gmail.com>
2020-08-14 08:57:46 +0200

    Updates

---
+++ precache-manifest.12cb8abb077c2f48f8efcf6ce3ea42c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6208a9cdf9a97c1fea811c97e862a04",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a1f91642c509363ed658",
+    "url": "/weather-app/static/css/main.ad4a22c2.chunk.css"
+  },
+  {
+    "revision": "6d4840c341acc3d366a7",
+    "url": "/weather-app/static/js/2.bb0e8705.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.bb0e8705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f91642c509363ed658",
+    "url": "/weather-app/static/js/main.f6615433.chunk.js"
+  },
+  {
+    "revision": "bd8931540699310e4243",
+    "url": "/weather-app/static/js/runtime-main.314cd1ff.js"
+  }
+]);

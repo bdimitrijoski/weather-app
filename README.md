59c9a5f66ecffe7b71032b7f249f5bdb210a8c01
Branko Dimitrijoski <brankodimitrioski@gmail.com>
2020-08-14 09:16:34 +0200

    updates with images, https content and gh pages

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Weather app
 
 A simple weather application that displays a weather forecast for Munich. Uses react-redux to manage state.
+
+Live Preview
+[https://bdimitrijoski.github.io/weather-app/](https://bdimitrijoski.github.io/weather-app/)
--- src/domain/services/weather-forecast-resource.service.ts
+++ src/domain/services/weather-forecast-resource.service.ts
@@ -4,7 +4,7 @@ export class WeatherForecastResourceService {
     getWeather(): Promise<WeatherData[]> {
         return new Promise((resolve, reject) => {
             const weatherAPIKey = '75f972b80e26f14fe6c920aa6a85ad57';
-            fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=${weatherAPIKey}&cnt=40&units=imperial`)
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=${weatherAPIKey}&cnt=40&units=imperial`)
                 .then(res => res.json())
                 .then(data => resolve(data.list));
         });
